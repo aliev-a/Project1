3f97f0b0450b535ba76dfc065205fd2b735768ac
A.Abubakr <57147605+aliev-a@users.noreply.github.com>
2026-04-14 17:58:19 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,70 +1,61 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import datasets
+from sklearn.datasets import fetch_california_housing
 from sklearn.ensemble import RandomForestRegressor
 
 st.write("""
-# Boston House Price Prediction App
+# California House Price Prediction App
 
-This app predicts the **Boston House Price**!
+This app predicts the **California House Price**!
 """)
 st.write('---')
 
-# Loads the Boston House Price Dataset
-boston = datasets.load_boston()
-X = pd.DataFrame(boston.data, columns=boston.feature_names)
-Y = pd.DataFrame(boston.target, columns=["MEDV"])
+# Load dataset
+housing = fetch_california_housing()
+X = pd.DataFrame(housing.data, columns=housing.feature_names)
+Y = pd.DataFrame(housing.target, columns=["PRICE"])
 
 # Sidebar
-# Header of Specify Input Parameters
 st.sidebar.header('Specify Input Parameters')
 
 def user_input_features():
-    CRIM = st.sidebar.slider('CRIM', X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())
-    ZN = st.sidebar.slider('ZN', X.ZN.min(), X.ZN.max(), X.ZN.mean())
-    INDUS = st.sidebar.slider('INDUS', X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
-    CHAS = st.sidebar.slider('CHAS', X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
-    NOX = st.sidebar.slider('NOX', X.NOX.min(), X.NOX.max(), X.NOX.mean())
-    RM = st.sidebar.slider('RM', X.RM.min(), X.RM.max(), X.RM.mean())
-    AGE = st.sidebar.slider('AGE', X.AGE.min(), X.AGE.max(), X.AGE.mean())
-    DIS = st.sidebar.slider('DIS', X.DIS.min(), X.DIS.max(), X.DIS.mean())
-    RAD = st.sidebar.slider('RAD', X.RAD.min(), X.RAD.max(), X.RAD.mean())
-    TAX = st.sidebar.slider('TAX', X.TAX.min(), X.TAX.max(), X.TAX.mean())
-    PTRATIO = st.sidebar.slider('PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
-    B = st.sidebar.slider('B', X.B.min(), X.B.max(), X.B.mean())
-    LSTAT = st.sidebar.slider('LSTAT', X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
-    data = {'CRIM': CRIM,
-            'ZN': ZN,
-            'INDUS': INDUS,
-            'CHAS': CHAS,
-            'NOX': NOX,
-            'RM': RM,
-            'AGE': AGE,
-            'DIS': DIS,
-            'RAD': RAD,
-            'TAX': TAX,
-            'PTRATIO': PTRATIO,
-            'B': B,
-            'LSTAT': LSTAT}
+    MedInc = st.sidebar.slider('MedInc (Median Income)', float(X.MedInc.min()), float(X.MedInc.max()), float(X.MedInc.mean()))
+    HouseAge = st.sidebar.slider('HouseAge', float(X.HouseAge.min()), float(X.HouseAge.max()), float(X.HouseAge.mean()))
+    AveRooms = st.sidebar.slider('AveRooms', float(X.AveRooms.min()), float(X.AveRooms.max()), float(X.AveRooms.mean()))
+    AveBedrms = st.sidebar.slider('AveBedrms', float(X.AveBedrms.min()), float(X.AveBedrms.max()), float(X.AveBedrms.mean()))
+    Population = st.sidebar.slider('Population', float(X.Population.min()), float(X.Population.max()), float(X.Population.mean()))
+    AveOccup = st.sidebar.slider('AveOccup', float(X.AveOccup.min()), float(X.AveOccup.max()), float(X.AveOccup.mean()))
+    Latitude = st.sidebar.slider('Latitude', float(X.Latitude.min()), float(X.Latitude.max()), float(X.Latitude.mean()))
+    Longitude = st.sidebar.slider('Longitude', float(X.Longitude.min()), float(X.Longitude.max()), float(X.Longitude.mean()))
+
+    data = {
+        'MedInc': MedInc,
+        'HouseAge': HouseAge,
+        'AveRooms': AveRooms,
+        'AveBedrms': AveBedrms,
+        'Population': Population,
+        'AveOccup': AveOccup,
+        'Latitude': Latitude,
+        'Longitude': Longitude
+    }
     features = pd.DataFrame(data, index=[0])
     return features
 
 df = user_input_features()
 
-# Main Panel
-
-# Print specified input parameters
+# Main panel
 st.header('Specified Input parameters')
 st.write(df)
 st.write('---')
 
-# Build Regression Model
+# Build model
 model = RandomForestRegressor()
-model.fit(X, Y)
-# Apply Model to Make Prediction
+model.fit(X, Y.values.ravel())
+
+# Prediction
 prediction = model.predict(df)
 
-st.header('Prediction of MEDV')
-st.write(prediction)
+st.header('Prediction of House Price')
+st.write(prediction[0])
 st.write('---')
